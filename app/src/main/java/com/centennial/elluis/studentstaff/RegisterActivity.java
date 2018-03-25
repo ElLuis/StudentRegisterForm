cbadc73334e6cc27f5383de4bc4b6c5a8f32d9c4
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2018-03-24 20:41:55 -0400

    Student class

--- app/src/main/java/com/centennial/elluis/studentstaff/RegisterActivity.java
+++ app/src/main/java/com/centennial/elluis/studentstaff/RegisterActivity.java
@@ -3,18 +3,44 @@ package com.centennial.elluis.studentstaff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText fName;
+    private EditText lName;
+    private EditText dob;
+    private EditText username;
+    private EditText password;
+
+    private EditText[] editTexts = {fName,lName,dob,username,password};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //initializing EditTexts
+        fName = findViewById(R.id.firstNameET);
+        lName = findViewById(R.id.lastNameET);
+        dob = findViewById(R.id.dobET);
+        username = findViewById(R.id.usernameET);
+        password = findViewById(R.id.passwordET);
+
+
     }
 
     public void submitBtn_OnClick(View view) {
+        //create instance
     }
 
     public void cancelBtn_OnClick(View view) {
+
+        //reset EditTexts
+        for (EditText et:editTexts
+             ) {
+            et.setText("");
+        }
+
     }
 }
---
+++ app/src/main/java/com/centennial/elluis/studentstaff/Student.java
@@ -0,0 +1,93 @@
+package com.centennial.elluis.studentstaff;
+
+
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Student {
+
+    private String fName;
+    private String lName;
+    private String dob;
+    private String username;
+    private String password;
+
+    private ArrayList<Student> students;
+
+    //Properties
+    public void setFirstName(String value)
+    {
+        fName = value;
+    }
+    public String getFirstName()
+    {
+        return fName;
+    }
+    public void setLastName(String value)
+    {
+        lName = value;
+    }
+    public String getLastName()
+    {
+        return lName;
+    }
+    public void setDob(String value)
+    {
+        dob = value;
+    }
+    public String getDob()
+    {
+        return dob;
+    }
+    public void setUsername(String value)
+    {
+        username = value;
+    }
+    public String getUsername()
+    {
+        return username;
+    }
+    public void setPassword(String value)
+    {
+        password = value;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+
+    //constructor
+    public Student(String firstName, String lastName, String dateOfBirth, String username, String password)
+    {
+        this.fName = firstName;
+        this.lName = lastName;
+        this.dob = dateOfBirth;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void AddStudent(Student newStudent)
+    {
+        students = new ArrayList<Student>() {
+
+        };
+
+        students.add(newStudent);
+        }
+
+    public Student getAllStudents() {
+        for (Student s : students
+                ) {
+            return s;
+        }
+        return null;
+    }
+
+    }
+
+
